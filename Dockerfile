a307b58f489e19066e9d828aca21232f1a30e309
Alex <alexandre.lambin@gmail.com>
2024-04-30 15:24:55 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+ARG DISTRIBUTION
+
+FROM debian:${DISTRIBUTION}-slim
+
+ENV ACTIVATION_CODE="xxxxxxx"
+ENV SERVER="smart"
+ENV HEALTHCHECK=""
+ENV BEARER=""
+ENV NETWORK="on"
+ENV PROTOCOL="lightway_udp"
+ENV CIPHER="chacha20"
+
+ARG NUM
+ARG PLATFORM
+ARG TARGETPLATFORM
+
+COPY files/ /expressvpn/
+
+RUN apt update && apt install -y --no-install-recommends \
+    expect curl ca-certificates iproute2 wget jq iptables iputils-ping
+
+RUN if [ "${TARGETPLATFORM}" = "linux/arm64" ]; then \
+    dpkg --add-architecture armhf \
+    && apt update && apt install -y --no-install-recommends \
+    libc6:armhf libstdc++6:armhf \
+    && cd /lib && ln -s arm-linux-gnueabihf/ld-2.23.so ld-linux.so.3; \
+    fi
+
+RUN wget -q https://www.expressvpn.works/clients/linux/expressvpn_${NUM}-1_${PLATFORM}.deb -O /expressvpn/expressvpn_${NUM}-1_${PLATFORM}.deb \
+    && dpkg -i /expressvpn/expressvpn_${NUM}-1_${PLATFORM}.deb \
+    && rm -rf /expressvpn/*.deb
+
+RUN apt-get purge --autoremove -y wget \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -rf /var/log/*.log
+
+HEALTHCHECK --start-period=1m --timeout=10s --interval=10m --retries=3 CMD bash /expressvpn/healthcheck.sh
+
+ENTRYPOINT ["/bin/bash", "/expressvpn/start.sh"]
---
+++ expressvpn/start.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ $AUTO_UPDATE = "on" ]]; then
+    DEBIAN_FRONTEND=noninteractive apt update && apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -y --only-upgrade expressvpn --no-install-recommends && apt autoclean && apt clean && apt autoremove && rm -rf /var/lib/apt/lists/* && rm -rf /var/log/*.log
+fi
+
+if [[ -f "/etc/resolv.conf" ]]; then
+    cp /etc/resolv.conf /etc/resolv.conf.bak
+    umount /etc/resolv.conf &>/dev/null
+    cp /etc/resolv.conf.bak /etc/resolv.conf
+    rm /etc/resolv.conf.bak
+fi
+
+sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
+
+output=$(service expressvpn restart 2>&1)
+if [[ $output == *"failed!"* ]]; then
+    echo "Service expressvpn restart failed!"
+    bash /expressvpn/start.sh
+    exit 1
+fi
+
+output=$(expect -f /expressvpn/activate.exp "$ACTIVATION_CODE")
+if [[ $output == *"Please activate your account."* ]]; then
+    echo "Activation failed!"
+    bash /expressvpn/start.sh
+    exit 1
+fi
+
+expressvpn preferences set preferred_protocol $PROTOCOL
+expressvpn preferences set lightway_cipher $CIPHER
+expressvpn preferences set send_diagnostics false
+expressvpn preferences set block_trackers true
+bash /expressvpn/uname.sh
+expressvpn preferences set auto_connect true
+expressvpn connect $SERVER
+
+for i in $(echo $WHITELIST_DNS | sed "s/ //g" | sed "s/,/ /g")
+do
+    iptables -A xvpn_dns_ip_exceptions -d ${i}/32 -p udp -m udp --dport 53 -j ACCEPT
+    echo "allowing dns server traffic in iptables: ${i}"
+done
+
+touch /var/log/temp.log
+tail -f /var/log/temp.log
+
+exec "$@"
